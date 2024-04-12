387a7aadcf7d13c266e40b7eb542a8bad32feeba
root <wangtongpy@foxmail.com>
2024-04-12 14:58:20 +0800

    update

--- cmd/win_helper/sub/init.go
+++ cmd/win_helper/sub/init.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/flosch/pongo2/v6"
 	"github.com/spf13/cobra"
+	"os"
 	"win_helper/pkg/project"
+	"win_helper/pkg/util/fileUtils"
 )
 
 var (
@@ -80,7 +82,7 @@ func newInitReadmeCmd() *cobra.Command {
 		Args:  validateInitReadmeCmd,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var tplExample = pongo2.Must(pongo2.FromBytes(initReadmeTemplate))
-			out, err := tplExample.Execute(
+			out, err := tplExample.ExecuteBytes(
 				pongo2.Context{
 					"djangoVersion": initDjangoVersion,
 					"pythonVersion": initPythonVersion,
@@ -89,6 +91,35 @@ func newInitReadmeCmd() *cobra.Command {
 			)
 			if err != nil {
 				return err
+			}
+			save := func(filename string, data []byte) error {
+				err := os.WriteFile(filename, data, 0644)
+				if err != nil {
+					return fmt.Errorf("写入服务失败。%v", err)
+				}
+				return nil
+			}
+			filename := "README.md"
+			if fileUtils.FileExist(filename) {
+				for {
+					fmt.Print("File exists. Do you want to continue? (yes/no): ")
+					var input string
+					fmt.Scanln(&input)
+					switch input {
+					case "yes", "1":
+						fmt.Println("Continuing...")
+						err := save(filename, out)
+						if err != nil {
+							return err
+						}
+					// 继续执行其他操作...
+					case "no", "0":
+						fmt.Println("Exiting...")
+						return nil
+					default:
+						fmt.Println("Invalid input. Please enter 'yes' or 'no'.")
+					}
+				}
 			}
 			fmt.Println(out)
 			return nil
